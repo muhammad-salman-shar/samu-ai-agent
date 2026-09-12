13b1ef31b8d224745d16b039f2d112aebc7a1480
Muhammad Salman <muhammadsalmansharbaloch@gmail.com>
2026-09-12 12:16:19 +0500

    Update settings_screen.dart

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'dart:convert';
+import '../services/accessibility_bridge.dart';
+import '../services/services.dart';
 import '../models/models.dart';
 
 /// Settings screen for configuring AI backend and permissions
